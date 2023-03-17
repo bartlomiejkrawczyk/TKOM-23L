910739a52054d48583dce113b0da53b32fd630b5
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-18 00:30:49 +0100

    Update main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import lombok.extern.slf4j.Slf4j;
+import org.example.ast.Expression;
 import org.example.error.ErrorHandlerImpl;
 import org.example.lexer.CommentFilterLexer;
+import org.example.lexer.Lexer;
 import org.example.lexer.LexerImpl;
+import org.example.parser.Parser;
+import org.example.parser.ParserImpl;
 import org.example.token.Token;
 import org.example.token.TokenType;
 
@@ -26,17 +30,35 @@ public class Main {
 		}
 	}
 
+
+	@SuppressWarnings({"unused", "java:S125"})
 	private static void handleStream(InputStream file) {
 		var reader = new InputStreamReader(file);
 		var errorHandler = new ErrorHandlerImpl();
 		var lexer = new LexerImpl(reader, errorHandler);
 		var filter = new CommentFilterLexer(lexer);
+		var parser = new ParserImpl(filter, errorHandler);
 
+		testLexer(lexer);
+		// testParser(parser);
+	}
+
+	@SuppressWarnings("unused")
+	private static void testLexer(Lexer lexer) {
 		Token token;
 		do {
-			token = filter.nextToken();
+			token = lexer.nextToken();
 			log.info("Token = {}", token);
 		} while (token.getType() != TokenType.END_OF_FILE);
+	}
+
+	@SuppressWarnings("unused")
+	private static void testParser(Parser parser) {
+		Expression expression;
+		do {
+			expression = parser.nextExpression();
+			log.info("Expression = {}", expression);
+		} while (expression != null);
 	}
 
 }
