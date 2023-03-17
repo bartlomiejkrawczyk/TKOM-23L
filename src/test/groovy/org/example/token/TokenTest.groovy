9560395e00e02fc82796bb542d265f6971baa477
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-17 17:31:48 +0100

    Add token tests

---
+++ src/test/groovy/org/example/token/TokenTest.groovy
@@ -0,0 +1,29 @@
+package org.example.token
+
+import org.example.token.type.FloatingPointToken
+import org.example.token.type.IntegerToken
+import org.example.token.type.KeywordToken
+import org.example.token.type.StringToken
+import spock.lang.Specification
+
+
+class TokenTest extends Specification {
+
+	var static position = Position.builder()
+			.line(0)
+			.characterNumber(0)
+			.build()
+
+	def 'Should return correct value'() {
+		expect:
+		token.getValue() == value
+
+		where:
+		token                                                                     || value
+		new StringToken(TokenType.IDENTIFIER, position, "value")                  || "value"
+		new IntegerToken(TokenType.INTEGER_CONSTANT, position, 1)                 || 1
+		new FloatingPointToken(TokenType.FLOATING_POINT_CONSTANT, position, 1.25) || 1.25
+		new KeywordToken(TokenType.FUNCTION_DEFINITION, position)                 || null
+	}
+
+}
