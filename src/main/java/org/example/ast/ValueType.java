d35ccb73e19f87056adeaac3a7dc995d2e095856
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-18 00:23:02 +0100

    Add "of" functionality to ValueType

--- src/main/java/org/example/ast/ValueType.java
+++ src/main/java/org/example/ast/ValueType.java
@@ -1,7 +1,9 @@
 package org.example.ast;
 
+import java.util.EnumSet;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,4 +14,13 @@ public enum ValueType {
 	;
 
 	private final String type;
+
+	public static ValueType of(String value) {
+		// TODO: Decide what to do when type is not found
+		return EnumSet.allOf(ValueType.class)
+				.stream()
+				.filter(it -> StringUtils.equalsIgnoreCase(it.getType(), value))
+				.findFirst()
+				.orElse(null);
+	}
 }
