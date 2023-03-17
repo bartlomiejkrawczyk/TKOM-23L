fa4b7893e0ed13c1954a386ac37c371a61f5aed9
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-17 01:04:21 +0100

    Add end of file reached exception

---
+++ src/main/java/org/example/lexer/error/EndOfFileReachedException.java
@@ -0,0 +1,23 @@
+package org.example.lexer.error;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import org.example.token.Position;
+
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@ToString
+public class EndOfFileReachedException extends RuntimeException {
+
+	String expectedEnclosingKeyword;
+	Position position;
+
+	public EndOfFileReachedException(String expectedEnclosingKeyword, Position position) {
+		super(String.format("String starting at %s is missing a closing character combination: %s", position, expectedEnclosingKeyword));
+		this.expectedEnclosingKeyword = expectedEnclosingKeyword;
+		this.position = position;
+	}
+
+}
