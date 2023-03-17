7b4b88c2d8ac39ba9981106fa17a5f33be9d8e0c
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-17 23:36:29 +0100

    Add numeric overflow exception

---
+++ src/main/java/org/example/lexer/error/NumericOverflowException.java
@@ -0,0 +1,21 @@
+package org.example.lexer.error;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import org.example.token.Position;
+
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@ToString
+public class NumericOverflowException extends RuntimeException {
+
+	Position position;
+
+	public NumericOverflowException(Position position) {
+		super(String.format("Number at %s has reached limit", position));
+		this.position = position;
+	}
+}
