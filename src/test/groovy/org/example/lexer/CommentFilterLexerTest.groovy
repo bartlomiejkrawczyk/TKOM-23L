398f00234a7598a3523908672cc270b2382e552a
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-17 17:25:32 +0100

    Add comment filter lexer tests

---
+++ src/test/groovy/org/example/lexer/CommentFilterLexerTest.groovy
@@ -0,0 +1,32 @@
+package org.example.lexer
+
+import org.example.token.Position
+import org.example.token.TokenType
+import org.example.token.type.StringToken
+import spock.lang.Specification
+
+
+class CommentFilterLexerTest extends Specification {
+
+	var position = Position.builder()
+			.line(0)
+			.characterNumber(0)
+			.build()
+
+	var comment = "Comment"
+
+	def 'Should filter comments correctly'() {
+		given:
+		var lexer = Mock(Lexer)
+		var filter = new CommentFilterLexer(lexer)
+
+		1 * lexer.nextToken() >> new StringToken(tokenType, position, comment)
+		1 * lexer.nextToken() >> new StringToken(TokenType.IDENTIFIER, position, comment)
+
+		expect:
+		filter.nextToken().getType() == TokenType.IDENTIFIER
+
+		where:
+		tokenType << LexerUtility.COMMENTS.values()
+	}
+}
