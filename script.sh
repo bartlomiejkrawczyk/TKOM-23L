9a9194f7634f359e439774f047749d67fb6ab3c3
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-22 12:41:36 +0100

    Add script for running the application

---
+++ script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+FAT_JAR="./build/libs/tkom-1.0-SNAPSHOT-all.jar"
+
+help () {
+    cat << EOF
+Usage: script.sh [OPTION] [FILE]
+    -h --help        Display this message
+    -c --clean       Re-build project from scratch before running application
+EOF
+exit 0;
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help)
+        help
+        ;;
+    -c|--clean)
+        ./gradlew clean
+        shift
+        ;;
+    -*)
+        echo "Unknown option $1" 1>&2
+        help
+        exit 1
+        ;;
+    *)
+        FILE="$1"
+        shift
+        ;;
+  esac
+done
+
+
+
+if [ ! -f "$FAT_JAR" ]; then
+  ./gradlew shadowJar
+fi
+
+java -jar "$FAT_JAR"
