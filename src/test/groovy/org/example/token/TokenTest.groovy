602356b29d955c2a0b18a4971eabacb2e1db264b
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-26 14:59:39 +0200

    Add precedence token tests

--- src/test/groovy/org/example/token/TokenTest.groovy
+++ src/test/groovy/org/example/token/TokenTest.groovy
@@ -20,7 +20,21 @@ class TokenTest extends Specification {
 		new IntegerToken(position, 1)                             || 1
 		new FloatingPointToken(position, 1.25)                    || 1.25
 		new KeywordToken(TokenType.FUNCTION_DEFINITION, position) || null
-		new BooleanToken(position, true) || true
+		new BooleanToken(position, true)                          || true
 	}
 
+	def 'Should return correct precedence'() {
+		expect:
+		token.getPrecedence() == value
+
+		where:
+		token                                                     || value
+		new StringToken(TokenType.IDENTIFIER, position, "value")  || -1
+		new IntegerToken(position, 1)                             || -1
+		new FloatingPointToken(position, 1.25)                    || -1
+		new KeywordToken(TokenType.FUNCTION_DEFINITION, position) || -1
+		new KeywordToken(TokenType.PLUS, position)                || TokenType.PLUS.getPrecedence()
+		new KeywordToken(TokenType.TIMES, position)               || TokenType.TIMES.getPrecedence()
+		new BooleanToken(position, true)                          || -1
+	}
 }
