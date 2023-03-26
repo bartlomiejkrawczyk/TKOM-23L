b3e00f9ee3bd9bb91262534992b82bcd732b3187
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-26 14:37:08 +0200

    Update error handler to format exceptions correctly

--- src/main/java/org/example/error/ErrorHandlerImpl.java
+++ src/main/java/org/example/error/ErrorHandlerImpl.java
@@ -3,71 +3,82 @@ package org.example.error;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.example.lexer.CharactersUtility;
+import org.example.lexer.LexerConfiguration;
+import org.example.lexer.PositionalReader;
 import org.example.lexer.PositionalReaderImpl;
 import org.example.lexer.error.LexerException;
 import org.example.parser.error.ParserException;
-import reactor.core.publisher.Flux;
+import org.example.token.Position;
 
 @Slf4j
 public class ErrorHandlerImpl implements ErrorHandler {
 
-	private static final int MAX_ERRORS = 500;
+	private final Map<Position, List<PositionalException>> exceptions = new HashMap<>();
 
-	private final List<LexerException> lexerExceptions = new ArrayList<>();
-	private final List<ParserException> parserExceptions = new ArrayList<>();
-
+	@SuppressWarnings("StatementWithEmptyBody")
 	@Override
 	public void showExceptions(Reader reader) throws IOException {
 		var positionalReader = new PositionalReaderImpl(reader);
-		var result = Flux.concat(
-						Flux.fromIterable(lexerExceptions),
-						Flux.fromIterable(parserExceptions)
-				)
-				.map(it -> Map.entry(it.getPosition(), it))
-				.groupBy(Map.Entry::getKey)
-				.flatMap(groupedExceptions ->
-						groupedExceptions.map(Map.Entry::getValue)
-								.collectList()
-								.map(exceptions -> Map.entry(groupedExceptions.key(), exceptions))
-				)
-				.collectMap(Map.Entry::getKey, Map.Entry::getValue)
-				.blockOptional()
-				.orElse(Map.of());
+		while (processLine(positionalReader)) ;
+	}
 
+	private boolean processLine(PositionalReader reader) throws IOException {
+		var lineBuilder = formatLine(reader);
+		var exceptionsOnLine = new ArrayList<PositionalException>();
 		while (true) {
-			var read = positionalReader.read();
-			if (read == -1) {
-				break;
+			var read = reader.read();
+			if (read == CharactersUtility.END_OF_FILE) {
+				logLine(lineBuilder.toString(), exceptionsOnLine);
+				return false;
+			}
+			var character = Character.toString(read);
+			if (StringUtils.equals(character, CharactersUtility.NEW_LINE)) {
+				logLine(lineBuilder.toString(), exceptionsOnLine);
+				return true;
+			} else if (lineBuilder.length() < LexerConfiguration.MAX_STRING_LENGTH) {
+				lineBuilder.append(character);
 			}
 
-			var currentPosition = positionalReader.getPosition();
-			var exceptions = result.getOrDefault(currentPosition, null);
+			var currentPosition = reader.getPosition();
+			var exceptionsAtPosition = exceptions.getOrDefault(currentPosition, List.of());
+			exceptionsOnLine.addAll(exceptionsAtPosition);
+		}
+	}
 
-			if (exceptions == null) {
-				continue;
-			}
-
-			for (var exception : exceptions) {
+	private void logLine(String line, List<PositionalException> exceptionsOnLine) {
+		if (!exceptionsOnLine.isEmpty()) {
+			log.info(line);
+			for (var exception : exceptionsOnLine) {
 				log.error(exception.getMessage());
 			}
 		}
 	}
 
+	private StringBuilder formatLine(PositionalReader reader) {
+		var line = String.format("%4d: ", reader.getLine() + 1);
+		return new StringBuilder(line);
+	}
+
 	@Override
 	public void handleLexerException(LexerException exception) {
-		lexerExceptions.add(exception);
-		if (lexerExceptions.size() > MAX_ERRORS) {
-			throw new TooManyExceptionsException();
-		}
+		handleException(exception);
 	}
 
 	@Override
 	public void handleParserException(ParserException exception) {
-		parserExceptions.add(exception);
-		if (parserExceptions.size() > MAX_ERRORS) {
+		handleException(exception);
+	}
+
+	private void handleException(PositionalException exception) {
+		var exceptionsAtPosition = exceptions.computeIfAbsent(exception.getPosition(), position -> new ArrayList<>());
+		exceptionsAtPosition.add(exception);
+		if (exceptions.size() > ErrorHandlerConfiguration.MAX_ERRORS) {
 			throw new TooManyExceptionsException();
 		}
 	}
