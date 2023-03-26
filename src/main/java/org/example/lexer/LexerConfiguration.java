198ac8fea57a8863f78ca896db984e770f6b298d
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-26 01:15:07 +0100

    Make max sizes configurable

--- src/main/java/org/example/lexer/LexerConfiguration.java
+++ src/main/java/org/example/lexer/LexerConfiguration.java
@@ -1,11 +1,19 @@
 package org.example.lexer;
 
+import java.util.Optional;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class LexerConfiguration {
 
 	public static final int BASE_TEN = 10;
-	public static final int MAX_IDENTIFIER_LENGTH = 100;
-	public static final int MAX_STRING_LENGTH = 1_000;
+	public static final int MAX_IDENTIFIER_LENGTH = getProperty("max.identifier.length", 100);
+	public static final int MAX_STRING_LENGTH = getProperty("max.string.length", 1_000);
+
+	private static int getProperty(String property, int defaultValue) {
+		var value = System.getProperty(property);
+		return Optional.ofNullable(value)
+				.map(Integer::parseInt)
+				.orElse(defaultValue);
+	}
 }
