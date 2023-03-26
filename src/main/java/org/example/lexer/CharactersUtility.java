5fc4c050e94113f712f64ddcda22fd87c81b5c4f
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-26 12:51:14 +0200

    Move new line character to CharactersUtility

--- src/main/java/org/example/lexer/CharactersUtility.java
+++ src/main/java/org/example/lexer/CharactersUtility.java
@@ -8,6 +8,7 @@ public class CharactersUtility {
 	public static final String UNDERSCORE = "_";
 	public static final String SPACE = " ";
 	public static final String DOT = ".";
+	public static final String NEW_LINE = "\n";
 	public static final int END_OF_FILE = -1;
 	public static final char ESCAPE_CHARACTER = '\\';
 	public static final char ZERO_CHARACTER = '0';
