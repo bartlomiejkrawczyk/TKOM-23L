3115f92cebe0985d1a57e135f44d2818864a13be
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-05-01 15:45:51 +0200

    Delete not commented out code

--- src/main/java/org/example/ast/expression/ArithmeticExpression.java
+++ src/main/java/org/example/ast/expression/ArithmeticExpression.java
@@ -3,17 +3,4 @@ package org.example.ast.expression;
 import org.example.ast.Expression;
 
 public interface ArithmeticExpression extends Expression {
-//
-//	TokenType operation;
-//	Expression leftExpression;
-//	Expression rightExpression;
-//
-//	@Override
-//	public Iterable<Node> getChildrenExpressions() {
-//		return Stream.of(
-//						leftExpression,
-//						rightExpression
-//				).map(it -> (Node) it)
-//				.toList();
-//	}
 }
