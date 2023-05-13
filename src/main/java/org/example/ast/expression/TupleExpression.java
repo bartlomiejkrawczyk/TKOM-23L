4754edcb4c74145e51649d852280932bd63cab9f
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-05-13 13:47:23 +0200

    Exclude expressions from toString method

--- src/main/java/org/example/ast/expression/TupleExpression.java
+++ src/main/java/org/example/ast/expression/TupleExpression.java
@@ -2,12 +2,14 @@ package org.example.ast.expression;
 
 import java.util.Map;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.Value;
 import org.example.ast.Expression;
 import org.example.ast.Node;
 import org.example.ast.type.TupleElement;
 import org.example.token.Position;
 
+@ToString(exclude = {"elements"})
 @EqualsAndHashCode(exclude = "position")
 @Value
 public class TupleExpression implements Expression {
