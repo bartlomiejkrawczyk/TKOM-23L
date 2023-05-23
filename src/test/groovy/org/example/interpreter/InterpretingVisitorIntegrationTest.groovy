0b32c4a117f420362ac5eb1a6451898d392c0bab
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-05-24 01:15:56 +0200

    Add tuple tests

--- src/test/groovy/org/example/interpreter/InterpretingVisitorIntegrationTest.groovy
+++ src/test/groovy/org/example/interpreter/InterpretingVisitorIntegrationTest.groovy
@@ -37,12 +37,13 @@ class InterpretingVisitorIntegrationTest extends Specification {
 		where:
 		program          || result
 		"hello.txt"      || "Hello, World!\n"
-		"math.txt"     || "10\n10\n108\n10\n2.5\n-123.25\n617.75\n20\n-120.75\n0\n125.75\n100\n-308.125\n1\n-0.02028397565922921\n-10\n-2.5\n"
+		"math.txt"       || "10\n10\n108\n10\n2.5\n-123.25\n617.75\n20\n-120.75\n0\n125.75\n100\n-308.125\n1\n-0.02028397565922921\n-10\n-2.5\n"
 		"logic.txt"      || "true\nfalse\ntrue\ntrue\nfalse\ntrue\nfalse\ntrue\ntrue\nfalse\ntrue\nfalse\nfalse\n"
-		"relation.txt" || "true\ntrue\nfalse\ntrue\nfalse\nfalse\ntrue\ntrue\nfalse\ntrue\nfalse\nfalse\nfalse\nfalse\nfalse\ntrue\ntrue\ntrue\nfalse\nfalse\nfalse\ntrue\ntrue\ntrue\ntrue\ntrue\ntrue\nfalse\nfalse\nfalse\nfalse\ntrue\n"
+		"relation.txt"   || "true\ntrue\nfalse\ntrue\nfalse\nfalse\ntrue\ntrue\nfalse\ntrue\nfalse\nfalse\nfalse\nfalse\nfalse\ntrue\ntrue\ntrue\nfalse\nfalse\nfalse\ntrue\ntrue\ntrue\ntrue\ntrue\ntrue\nfalse\nfalse\nfalse\nfalse\ntrue\n"
 		"visibility.txt" || "2\n1\n3\n2\n1\n4\n1\n5\n2\n"
 		"if.txt"         || "if\nelse if\nelse\n"
 		"while.txt"      || "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n8\n6\n4\n2\n"
-		"map.txt"      || "false\ntrue\nid_1\nid_2\nid_3\nid_4\nid_4\nid_3\nid_2\n"
+		"map.txt"        || "false\ntrue\nid_1\nid_2\nid_3\nid_4\nid_4\nid_3\nid_2\n"
+		"tuple.txt"      || "abc\n1\ntrue\n"
 	}
 }
